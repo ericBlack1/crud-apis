7e2da562329847eaa3fa74d2e839e2d16b96a5ff
ericBlack1 <ericangeloawa@gmail.com>
2024-08-10 01:25:23 +0100

    crud api

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const productRoute = require('./routes/product.route')
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-})
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
-})
+});
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('Connected to MongoDB database');
+    } catch (error) {
+        console.error('Connection to MongoDB failed', error);
+        process.exit(1);
+    }
+};
+
+const startServer = async () => {
+    await connectDB();
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+};
+
+startServer();
